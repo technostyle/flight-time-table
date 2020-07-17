572ead4adf9583d49c85737ceb59509b2ed0389b
dm.perlov <dm.perlov@jet.su>
2020-07-17 13:10:45 +0300

    basic set-up react semantic-ui

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	entry: './app/index.jsx',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					{ loader: "css-loader", options: { modules: true } }
+				]
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			}
+		]
+	},
+	devServer: {
+		contentBase: 'dist'
+	},
+	resolve: {
+		modules: ["app", "node_modules"],
+		extensions: [".js", ".jsx"]
+	}
+}
